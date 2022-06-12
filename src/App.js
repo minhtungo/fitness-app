d7f41bf3088f15ab4a1d17ca361554f54b08dc58
minhtungo <loducfc@gmail.com>
2022-06-12 01:22:43 -0400

    styled hero banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/exercise/:id' element={<ExerciseDetail />}></Route>
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
+export default App;
